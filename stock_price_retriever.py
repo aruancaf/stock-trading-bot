6c0d0defe9ffa36a6e9bcdb5bb4eeb0f8eb6a1ab
Aditya-Paly <aoberai05@gmail.com>
2020-05-19 00:40:56 -0700

    gets most recent msft data in python

--- stock_price_retriever.py
+++ stock_price_retriever.py
@@ -1,18 +1,6 @@
-from datetime import date
-
 import yfinance as yf
-from pandas_datareader import data as pdr
 
-yf.pdr_override()
-import pandas as pd
+msft = yf.Ticker("AAPL")
 
-# Tickers list
-# We can add and delete any ticker from the list to get desired ticker live data
-ticker_list = ['DJIA', 'DOW', 'LB', 'EXPE', 'PXD', 'MCHP', 'CRM', 'JEC', 'NRG', 'HFC', 'NOW']
-today = date.today()
-
-files = []
-
-for ticker in ticker_list:
-    print("\n" + ticker + " stock prices\n")
-    print(yf.Ticker(ticker).history("1y"))
+# get stock info
+print(msft.history("1d").iloc[0])
