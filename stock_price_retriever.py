fca8afff6d62dd53ce8680235ac26fe5aa6aa189
Aditya-Paly <aoberai05@gmail.com>
2020-05-20 02:16:10 -0700

    gives ability to certain stocks which are stored in a dictionary for temporary testing purposes

--- stock_price_retriever.py
+++ stock_price_retriever.py
@@ -2,15 +2,21 @@ import yfinance as yf
 
 import yf_extender
 import yf_web_scraper
+import stock_portfolio
 
 while True:
     for stk in yf_web_scraper.get_most_actives():
         ticker_stock = yf.Ticker(stk)
         stk_price = ticker_stock.history("1d").iloc[0]['Close']
-        #     get stock info
+
+        # get stock info
         print("{0} price: {1}".format(stk, stk_price))
         previous_2mo_high = yf_extender.previous_2mo_high(ticker_stock)
-        print("Previous2MoHigh: {0} CurrentStockPrice: {1} ".format(previous_2mo_high, stk_price))
+        # print("Previous2MoHigh: {0} CurrentStockPrice: {1} ".format(previous_2mo_high, stk_price))
         if previous_2mo_high < stk_price:
-            print(ticker_stock.get_info())
-            print("buy!!!!")
+            if stk not in stock_portfolio.stocks:
+                print(ticker_stock.get_info())
+                stock_portfolio.stocks.update({stk: stk_price})
+                print(stock_portfolio.stocks)
+
+
