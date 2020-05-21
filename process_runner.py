2431d5c16e2198a6b53be8db8505ef804a718d64
Aditya-Paly <aoberai05@gmail.com>
2020-05-21 07:31:37 -0700

    added it such that it webscrapes multiple yahoo finance screeners and now trendfollowing is calculated on 4 different threads for speed purposes

--- process_runner.py
+++ process_runner.py
@@ -1,6 +1,26 @@
+import json
+
+import yfinance as yf
+from thread6 import thread6
+
+import portfolio_manager
 import trading_strategies
 import yf_web_scraper
 
+with open('stock_portfolio.json', "r+") as file:
+    portfolio_manager.stocks = json.load(file)
+positionPolarity = 0
+for i in portfolio_manager.stocks:
+    print("{0} {1}".format(i, yf.Ticker(i).history("1d").iloc[0]['Close'] - portfolio_manager.stocks[i]['Close']))
+    positionPolarity += yf.Ticker(i).history("1d").iloc[0]['Close'] - portfolio_manager.stocks[i]['Close']
+print("Position Polarity : {0}".format(positionPolarity))
 while True:
     most_active_stocks = yf_web_scraper.get_most_actives()
-    trading_strategies.trend_following(most_active_stocks)
+    thread6.run_threaded(trading_strategies.trend_following, most_active_stocks[0:round(len(most_active_stocks) / 4)])
+    thread6.run_threaded(trading_strategies.trend_following,
+                         most_active_stocks[round(len(most_active_stocks) / 4 + 1): round(len(most_active_stocks) / 2)])
+    thread6.run_threaded(trading_strategies.trend_following, most_active_stocks[
+                                                             round(len(most_active_stocks) / 2 + 1): round(
+                                                                 3 * len(most_active_stocks) / 4)])
+    thread6.run_threaded(trading_strategies.trend_following,
+                         most_active_stocks[round(3 * len(most_active_stocks) / 4 + 1):len(most_active_stocks)])
--- yf_web_scraper.py
+++ yf_web_scraper.py
@@ -3,28 +3,43 @@ from bs4 import BeautifulSoup
 
 
 def get_most_actives() -> []:
-    active_stocks_p1 = []
-    active_stocks_p2 = []
+    # active_stocks_p1 = []
+    # active_stocks_p2 = []
+    # headers = {
+    #     'Referer': 'https://itunes.apple.com',
+    #     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
+    # }
+    #
+    # page1 = requests.get("https://finance.yahoo.com/screener/predefined/most_actives?offset=0&count=100", headers)
+    # page2 = requests.get("https://finance.yahoo.com/screener/predefined/most_actives?count=100&offset=100", headers)
+    # soup1 = BeautifulSoup(page1.content, 'html.parser')
+    # soup2 = BeautifulSoup(page2.content, 'html.parser')
+    #
+    # for element in range(len(soup1.find_all('a'))):
+    #     active_stocks_p1.append(soup1.find_all('a')[element].get_text())
+    #
+    # for element in range(len(soup2.find_all('a'))):
+    #     active_stocks_p2.append(soup2.find_all('a')[element].get_text())
+    #
+    # lower_bound_p1 = active_stocks_p1.index("Heatmap View") + 1
+    # higher_bound_p1 = active_stocks_p1.index("Finance")
+    #
+    # lower_bound_p2 = active_stocks_p2.index("Heatmap View") + 1
+    # higher_bound_p2 = active_stocks_p2.index("Finance")
+
+    active_stocks = []
     headers = {
         'Referer': 'https://itunes.apple.com',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
     }
+    urls = ["https://finance.yahoo.com/screener/predefined/most_actives?offset=0&count=100", "https://finance.yahoo.com/screener/predefined/most_actives?count=100&offset=100", "https://finance.yahoo.com/screener/predefined/day_gainers", "https://finance.yahoo.com/screener/predefined/undervalued_growth_stocks"]
 
-    page1 = requests.get("https://finance.yahoo.com/screener/predefined/most_actives?offset=0&count=100", headers)
-    page2 = requests.get("https://finance.yahoo.com/screener/predefined/most_actives?count=100&offset=100", headers)
-    soup1 = BeautifulSoup(page1.content, 'html.parser')
-    soup2 = BeautifulSoup(page2.content, 'html.parser')
+    for url in urls:
+        soup = BeautifulSoup(requests.get(url, headers).content, 'html.parser')
+        temp_list = []
+        for element in soup.find_all('a'):
+            temp_list.append(element.get_text())
+        active_stocks += temp_list[temp_list.index("Heatmap View") + 1 : temp_list.index("Finance")]
 
-    for element in range(len(soup1.find_all('a'))):
-        active_stocks_p1.append(soup1.find_all('a')[element].get_text())
-
-    for element in range(len(soup2.find_all('a'))):
-        active_stocks_p2.append(soup2.find_all('a')[element].get_text())
-
-    lower_bound_p1 = active_stocks_p1.index("Heatmap View") + 1
-    higher_bound_p1 = active_stocks_p1.index("Finance")
-
-    lower_bound_p2 = active_stocks_p2.index("Heatmap View") + 1
-    higher_bound_p2 = active_stocks_p2.index("Finance")
-
-    return active_stocks_p1[lower_bound_p1:higher_bound_p1] + active_stocks_p2[lower_bound_p2:higher_bound_p2]
+    return active_stocks
+    # return active_stocks_p1[lower_bound_p1:higher_bound_p1] + active_stocks_p2[lower_bound_p2:higher_bound_p2]
