bb364706eaacdf83176d30060d4054ba77f608d1
aoberai <aoberai05@gmail.com>
2020-06-19 14:56:19 -0700

    run.sh works on windows with wsl from command prompt and on mac os

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-if [ "$(uname)" == "Darwin" ] || [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # Do something under Unix platform
-    echo "Unix"
+if [ "$(uname)" == "Darwin" ]; then
+    echo "MacOS"
     source ./venv/bin/activate
     ./process_runner.py
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    echo "Windows WSL"
-    source ./venv_windows/Scripts/activate
+    echo "Windows"
+    ./venv_windows/Scripts/activate
     ./venv_windows/Scripts/python process_runner.py
 fi
