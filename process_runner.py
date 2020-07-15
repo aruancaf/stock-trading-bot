f7ded22eb1ac2aa96c19cc7ea48267a2dc38cae2
aoberai <aoberai05@gmail.com>
2020-07-15 07:25:24 -0700

    made further fixes

--- process_runner.py
+++ process_runner.py
@@ -4,9 +4,10 @@ import portfolio_manager
 import trading_strategies
 import web
 import yf_web_scraper
-from utils import multithreading, json_simplifier as json_simp
+from utils import multithreading
 import trading_constants as const
 
+first_run = True
 portfolio_manager.refresh_account_balance()
 multithreading.run_thread(web.init_web)
 
@@ -14,4 +15,9 @@ while True:
     most_active_stocks = yf_web_scraper.get_active_tickers()
     print(most_active_stocks)
     portfolio_manager.print_account_status()
-    multithreading.run_chunked_threads(most_active_stocks, trading_strategies.run_stock_pipelines, const.trading_strategy_thread_count)
+    if first_run is True:
+        multithreading.run_thread(trading_strategies.evaluate_purchased_stocks)
+        first_run = False
+    multithreading.run_chunked_threads(most_active_stocks, trading_strategies.run_stock_pipelines,
+                                       const.trading_strategy_thread_count)
+
