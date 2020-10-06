16d6ae99fca07f4cbb89077eba78c9132e357780
Aditya-Paly <aoberai05@gmail.com>
2020-10-06 12:44:01 -0700

    reset portfolio

--- trading_constants.py
+++ trading_constants.py
@@ -1,6 +1,6 @@
 stop_loss = 0.12
 ema_cross_threshold = 0.02
 max_investment_partition = 0.3
-starting_account_value = 3000
+starting_account_value = 100000
 trading_strategy_thread_count = 45
 blacklist = ['BTC-USD', '^N225', '^AORD', 'ZIONW', 'TCEHY', 'JMPLF', 'FMCC', 'CICHF', 'CUK', 'EBCOY', 'C38U.SI', 'AGM-A', 'DNNGY', 'AOBC', 'DKNG', 'VST', 'TIVO']
