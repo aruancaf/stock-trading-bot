8dea3a133954cff48133b944b41373635598a571
Aditya Oberai <aoberai05@gmail.com>
2020-12-19 21:35:22 -0800

    added framework for threading

---
+++ runner.py
@@ -0,0 +1,11 @@
+import scraper
+import threading
+
+print(scraper.active_stocks())
+
+purchased_stocks_thread = threading.Thread(target=[Insert], args(1, )))
+stock_analysis_thread = threading.Thread(target=[Insert], args(1, )))
+
+purchased_stocks_thread.start()
+stock_analysis_thread.start()
+
--- scraper.py
+++ scraper.py
@@ -4,11 +4,13 @@ import requests
 
 urls = ["https://finance.yahoo.com/gainers", "https://finance.yahoo.com/screener/predefined/undervalued_growth_stocks", "https://finance.yahoo.com/screener/predefined/day_gainers", "https://finance.yahoo.com/screener/predefined/growth_technology_stocks", "https://finance.yahoo.com/screener/predefined/most_actives", "https://finance.yahoo.com/screener/predefined/undervalued_large_caps", "https://finance.yahoo.com/screener/predefined/aggressive_small_caps", "https://finance.yahoo.com/screener/predefined/small_cap_gainers"]
 
-for url in urls:
-    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-    results = soup.find(id="Lead-5-ScreenerResults-Proxy")
-    assets = soup.get_text()
-
-    for result in results.find_all('a'):
-        href = result['href']
-        if '=' in href: print(href[href.find('=') + 1:])
+def active_stocks():
+    scraped_stocks = []
+    for url in urls:
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+        results = soup.find(id="Lead-5-ScreenerResults-Proxy")
+        assets = soup.get_text()
+        for result in results.find_all('a'):
+            href = result['href']
+            if '=' in href: scraped_stocks.append(href[href.find('=') + 1:])
+    return scraped_stocks
