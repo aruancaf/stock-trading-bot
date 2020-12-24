187ba35317a1473a3293c65a7d8f0409596e0ded
Aditya Oberai <aoberai@pop-os.localdomain>
2020-12-24 08:09:09 -0800

    added better stock plotting

--- display_stocks.py
+++ display_stocks.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 import stock_data_gatherer as sdg
+import stock_analysis
 stock_prices = []
-for i in range(-5, 0):
-    stock_prices.append(sdg.get_historical_data("AAPL", "1d", "1m").iloc[-i].to_dict()['Close'])
-plt.scatter([0, 1, 2, 3, 4], stock_prices)
+n = 90
+for i in range(-n,0,1):
+    stock_prices.insert(0, sdg.get_historical_data("AAPL", "1d", "1m").iloc[-i].to_dict()['Close'])
+plt.plot(list(range(n)), stock_prices, 'xb-')
+# plt.ylim([130, 134])
+print("Price Slope: ", sdg.get_price_slope("AAPL"))
+stock_analysis
 plt.show()
 
