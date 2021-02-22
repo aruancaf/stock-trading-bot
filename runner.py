e5ea35fd9711cc002fccaa25aef2da3ac7e4c482
aoberai <aoberai05@gmail.com>
2021-02-21 22:39:35 -0800

    made fix which prevents script from ever quiting as a result of any weird https exceptions - just restarts script

--- runner.py
+++ runner.py
@@ -1,3 +1,6 @@
+# make buy sell distinction between stocks purchased for daytrading and long term purchases
+
+
 import scraper
 import threading
 import time
@@ -15,26 +18,28 @@ def daytrading_stock_analyzer(stocks):
     for stock_ticker in stocks: #purchases stocks based on daytrading patterns
         try:
             stock_score = 0
-            print("Analyzing", stock_ticker)
             stock_score += sa.moving_average_checker(stock_ticker)
             stock_score += sa.volume_checker(stock_ticker)
             if stock_score >= 0.2 and stock_ticker not in all_active_positions.keys():
                 alpaca.create_order(stock_ticker, 1) #todo: calculate order amount
                 active_positions_to_check[stock_ticker] = sdg.get_current_stock_data(stock_ticker)['Close']
                 all_active_positions[stock_ticker] = sdg.get_current_stock_data(stock_ticker)['Close']
-                print("Based on daytrading pattern analysis, buying", stock_ticker)
+                print("Based on daytrading pattern analysis, buying", stock_ticker, "Stock Score: ", stock_score)
         except Exception as e:
             pass
 
 def news_stock_analyzer(stock_ticker):
-    stock_score = 0
-    stock_score += nc.sentiment_analyzer(news.get_news(stock_ticker))
-    print(stock_ticker, "news score:", stock_score)
-    if stock_score >= 0.35 and stock_ticker not in all_active_positions.keys():
-        alpaca.create_order(stock_ticker, 1) #todo: calculate order amount
-        active_positions_to_check[stock_ticker] = sdg.get_current_stock_data(stock_ticker)['Close']
-        all_active_positions[stock_ticker] = sdg.get_current_stock_data(stock_ticker)['Close']
-        print("Based on News analysis, buying", stock_ticker)
+    try:
+        stock_score = 0
+        stock_score += nc.sentiment_analyzer(news.get_news(stock_ticker))
+        print(stock_ticker, "news score:", stock_score)
+        if stock_score >= 0.35 and stock_ticker not in all_active_positions.keys():
+            alpaca.create_order(stock_ticker, 1) #todo: calculate order amount
+            active_positions_to_check[stock_ticker] = sdg.get_current_stock_data(stock_ticker)['Close']
+            all_active_positions[stock_ticker] = sdg.get_current_stock_data(stock_ticker)['Close']
+            print("Based on News analysis, buying", stock_ticker)
+    except Exception as e:
+        print("News analysis not working for", stock_ticker)
 
 
 def stock_position_analyzer():
@@ -63,32 +68,29 @@ if __name__ == "__main__":
     positions = alpaca.get_positions()
     for position in positions: #todo also add orders
         active_positions_to_check[position.symbol] = float(position.cost_basis) #cost basis not working well
-                                                                                # rescanning of stocks is breaking system
 
     all_active_positions = active_positions_to_check.copy()
     print("Currently Purchased:", active_positions_to_check)
-
-    time.sleep(2)
     first_time_run = True
 
     while True:
-        current_time = datetime.now().strftime("%H:%M")
-        current_time = "12:01"
-        if current_time > const.STOCK_MARKET_OPEN_TIME and current_time < const.STOCK_MARKET_CLOSE_TIME:
-            if first_time_run:
-                threading.Thread(target=stock_position_analyzer).start()
-                first_time_run = False
-            active_stocks = scraper.active_stocks()
-            partitioned_stocks = util.partition_array(active_stocks, const.STOCK_SCANNER_PARTITION_COUNT)
-            for partition in partitioned_stocks:
-                threading.Thread(target=daytrading_stock_analyzer, args=[partition]).start()
-
-            for stock_ticker in const.STOCKS_TO_CHECK: #purchases stocks based on news info
-                threading.Thread(target=news_stock_analyzer, args=(stock_ticker,)).start()
-
-        else:
-            print("Market Close")
-            alpaca.sell_all_positions()
-            time.sleep(3600)
-
-
+        try:
+            print("New Iteration of Stock Scanning")
+            current_time = datetime.now().strftime("%H:%M")
+            # current_time = "12:01"
+            if current_time > const.STOCK_MARKET_OPEN_TIME and current_time < const.STOCK_MARKET_CLOSE_TIME:
+                if first_time_run:
+                    threading.Thread(target=stock_position_analyzer).start()
+                    first_time_run = False
+                active_stocks = scraper.active_stocks()
+                partitioned_stocks = util.partition_array(active_stocks, const.STOCK_SCANNER_PARTITION_COUNT)
+                for partition in partitioned_stocks:
+                    threading.Thread(target=daytrading_stock_analyzer, args=[partition]).start()
+            else:
+                alpaca.sell_all_positions()
+                print("Market Close")
+                for stock_ticker in const.STOCKS_TO_CHECK: #purchases stocks based on news info
+                    threading.Thread(target=news_stock_analyzer, args=(stock_ticker,)).start()
+                time.sleep(360000)
+        except Exception as e:
+            print("Restarting")
