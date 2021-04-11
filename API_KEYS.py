871613d7e5cf9641ee523a89c41431472891c0ba
Aditya Oberai <aoberai05@gmail.com>
2021-04-10 19:26:46 -0700

    Update API_KEYS.py

--- API_KEYS.py
+++ API_KEYS.py
@@ -1,2 +1,4 @@
+Replace these with your actual key id and secret key - these dont work
+
 TRADE_API_KEY_ID = 'PK4EQLI66YDOFWWOQ99Y'
 TRADE_API_SECRET_KEY = 'BQPJx7n1gyKpz349kG9fNtbb3DrdfJ900T81jB6m'
